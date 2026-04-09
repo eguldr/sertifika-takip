bb456c425ce91507c2a10548b214d8a820b0311d
eguldr <eguldr@gmail.com>
2026-04-09 18:26:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from datetime import datetime, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 
-app = Flask(__name__)
+db = SQLAlchemy()
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'gizli-anahtar-123')
 uri = os.environ.get('DATABASE_URL')
 if uri and uri.startswith("postgres://"):
@@ -18,6 +18,9 @@ login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
@@ -117,5 +120,4 @@ def logout():
         logout_user()
         return redirect(url_for('login'))
 
-with app.app_context():
-        db.create_all()
+
