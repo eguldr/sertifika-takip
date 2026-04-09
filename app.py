458fe197ba4e674439b66e4079d9659608fd32a0
eguldr <eguldr@gmail.com>
2026-04-09 16:21:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 with app.app_context():
     db.create_all()
-with app.app_context():
-    db.drop_all()
-    db.create_all()
 
 login_manager = LoginManager()
 login_manager.login_view = 'login'
