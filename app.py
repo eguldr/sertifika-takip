7292e4226b90163a54b39447ee014bef4010d900
eguldr <eguldr@gmail.com>
2026-04-24 17:35:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -367,7 +367,8 @@ def upload_belge(entry_id):
     if f:
         try:
             # En sade hali, imza hatası riskini sıfıra indirir
-            res = cloudinary.uploader.upload(f)
+            # İmza gerektirmeyen en garanti yöntem
+            res = cloudinary.uploader.unsigned_upload(f, upload_preset='ml_default')
             e   = Entry.query.get(entry_id)
             if e and (e.user_id == current_user.id or current_user.email == 'erhanadea@gmail.com'):
                 raw_url = res.get('secure_url')
