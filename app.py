d11bba093606878e0b90b0e94dcdeafc3f83808a
eguldr <eguldr@gmail.com>
2026-04-24 15:56:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,9 +42,9 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
  
 cloudinary.config(
-    cloud_name='dh2pefkko',  # Sonuna 'o' harfini EKLEDİK (Görseldeki gibi)
-    api_key='414697559795627',
-    api_secret='0q2xexoiKr25EeuI6CmFF8CXf2c'
+    cloud_name='dh2pefkko',          # Sonu 'o' ile bitecek (Panelde teyit ettik)
+    api_key='414697559795627',       # Moderation API Key'in
+    api_secret='0q2xexoiKr25EeuI6CmFF8CXf2c' # Word'e yapıştırdığın tam şifre
 )
  
  
